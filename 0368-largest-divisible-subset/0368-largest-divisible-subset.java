fcb423d0b2631bbca338015614ee3d65f43b7beb
Ankur357 <93982657+Ankur357@users.noreply.github.com>
2023-03-28 09:45:26 +0530

    Time: 27 ms (22.69%), Space: 43.2 MB (15.78%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,31 +1,25 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-              int n = nums.length;
-        int[] count = new int[n];
-        int[] pre = new int[n];
+        List<Integer> li = new ArrayList<>();
+        if(nums== null || nums.length==0) return li;
         Arrays.sort(nums);
-        int max = 0, index = -1;
-        for (int i = 0; i < n; i++) {
-            count[i] = 1;
-            pre[i] = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] % nums[j] == 0) {
-                    if (1 + count[j] > count[i]) {
-                        count[i] = count[j] + 1;
-                        pre[i] = j;
-                    }
+        List<List<Integer>> dp = new ArrayList<>();
+        for(int num: nums) dp.add(new ArrayList<>());
+        for(int i=0;i<nums.length;i++){
+            List<Integer> max = new ArrayList<>();
+            for(int k=0;k<i;k++){
+                if(nums[i]%nums[k]==0 && max.size()<dp.get(k).size()){
+                    max = dp.get(k);
                 }
             }
-            if (count[i] > max) {
-                max = count[i];
-                index = i;
+            dp.get(i).addAll(max);
+            dp.get(i).add(nums[i]);
+        }
+        for(int i=0;i<dp.size();i++){
+            if(li.size()<dp.get(i).size()){
+                li = dp.get(i);
             }
         }
-        List<Integer> res = new ArrayList<>();
-        while (index != -1) {
-            res.add(nums[index]);
-            index = pre[index];
-        }
-        return res;
+        return li;
     }
 }
