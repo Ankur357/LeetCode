bc7acc60d2599166751d2433b203ce10eed47d49
Ankur357 <93982657+Ankur357@users.noreply.github.com>
2023-07-07 21:43:45 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int arr[], int n)
+    {
+        // code here 
+        int low =0, mid=0, high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int tmp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = tmp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1) mid++;
+            else {
+                int tmp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = tmp;
+                high--;
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
