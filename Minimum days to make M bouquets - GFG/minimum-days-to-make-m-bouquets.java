a53c8bd755a37304b445072871851b2d405b3f52
Ankur Gautam <93982657+Ankur357@users.noreply.github.com>
2023-07-27 19:23:35 +0530

    Added solution - LeetHub

---
+++ Minimum days to make M bouquets - GFG/minimum-days-to-make-m-bouquets.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+       
+        int t = sc.nextInt();
+        while(t-- > 0){
+            
+            
+           int M = sc.nextInt();
+           int K = sc.nextInt();
+            
+            
+            //int n = sc.nextInt();
+            sc.nextLine();
+            String s[]=sc.nextLine().split(" ");
+            int n=s.length;
+            int[] bloomDay = new int[n];
+            for(int i=0;i<n;i++) bloomDay[i]=Integer.parseInt(s[i]);
+            
+            Solution obj = new Solution();
+            int res = obj.solve(M, K, bloomDay);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static int solve(int m, int k, int[] arr) {
+       long val = (long) m * k;
+        int n = arr.length; // Size of the array
+        if (val > n) return -1; // Impossible case.
+        // Find maximum and minimum:
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        // Apply binary search:
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static boolean possible(int[] arr, int day, int m, int k) {
+        int n = arr.length; // Size of the array
+        int cnt = 0;
+        int noOfB = 0;
+        // Count the number of bouquets:
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            } else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+}
