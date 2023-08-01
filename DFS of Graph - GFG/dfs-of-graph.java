f4ccf88d1e107c4203fd2c818bf3cf09dcb34d80
Ankur Gautam <93982657+Ankur357@users.noreply.github.com>
2023-08-01 12:00:30 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited = new boolean[V];
+        ArrayList<Integer> result = new ArrayList<>();
+        dfsRecursive(0, adj, visited, result);
+        return result;
+    }
+    private void dfsRecursive(int vertex, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> result) {
+        visited[vertex] = true;
+        result.add(vertex);
+
+        for (int adjVertex : adj.get(vertex)) {
+            if (!visited[adjVertex]) {
+                dfsRecursive(adjVertex, adj, visited, result);
+            }
+        }
+    }
+}
