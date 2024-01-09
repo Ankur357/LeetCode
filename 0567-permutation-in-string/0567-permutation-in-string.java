68b9d02e0d613bb24e6145f9b2179a478d0b5df7
Ankur Gautam <93982657+Ankur357@users.noreply.github.com>
2024-01-09 14:45:32 +0530

    Time: 8 ms (62.87%), Space: 42.6 MB (51.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,26 +1,16 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        int[] arr1 = new int[26];
         for(int i=0;i<s1.length();i++){
-            map.put(s1.charAt(i),map.getOrDefault(s1.charAt(i),0)+1);
+            arr1[s1.charAt(i)-'a']++;
         }
-        int i=0;
-        int j=0;
-        int count= map.size();
-        while(j<s2.length()){
-            if(map.containsKey(s2.charAt(j))){
-                map.put(s2.charAt(j),map.get(s2.charAt(j))-1);
-                if(map.get(s2.charAt(j))==0) count--;;
-           }
-            if(j-i+1==s1.length()){
-                if(count==0) return true;
-                if(map.containsKey(s2.charAt(i))){
-                    map.put(s2.charAt(i),map.get(s2.charAt(i))+1);
-                    if(map.get(s2.charAt(i))==1) count++;
-                }
-                i++;
+        int[] arr2 = new int[26];
+        for(int i=0;i<s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+            if(i>=s1.length()){
+                arr2[s2.charAt(i-s1.length())-'a']--;
             }
-            j++;
+            if(Arrays.equals(arr1,arr2)) return true;
         }
         return false;
     }
