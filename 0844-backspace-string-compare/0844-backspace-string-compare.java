e9e668a72cb8354e2be4a68e29aae6e2a6fead2e
Ankur Gautam <93982657+Ankur357@users.noreply.github.com>
2024-01-15 16:00:21 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (44.20%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        String s1 = check(s);
-        String s2 = check(t);
-        return s1.equals(s2);
-    }
-    public static String check(String s){
-        StringBuilder sb= new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='#') sb.append(s.charAt(i));
-            else if(sb.length()!=0) sb.deleteCharAt(sb.length()-1);
+        int index1 = s.length()-1;
+        int index2 = t.length()-1;
+        while(index1>=0 || index2>=0){
+            int n1 = checkCharIndex(s,index1);
+            int n2 = checkCharIndex(t,index2);
+            if(n1<0 && n2<0) return true;
+            if(n1<0 || n2<0) return false;
+            if(s.charAt(n1)!=t.charAt(n2)) return false;
+            index1 = n1-1;
+            index2 = n2-1;
         }
-        return sb.toString();
+        return true;
+    }
+    static int checkCharIndex(String str, int index){
+        int count=0;
+        while(index>=0){
+            if(str.charAt(index)=='#') count++;
+            else if(count>0) count--;
+            else break;
+            index--;
+        }
+        return index;
     }
 }
