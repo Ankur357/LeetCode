e67e354e71512ba33f1d5e185a9bde057c9b4479
Ankur Gautam <93982657+Ankur357@users.noreply.github.com>
2024-01-25 17:12:30 +0530

    Time: 176 ms (10.45%), Space: 57 MB (57.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,27 +1,47 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-      int area = 0, n = heights.length;
-      int start;
-      Stack<Pair<Integer,Integer>> stack = new Stack<>();
-      for(int i=0;i<heights.length;i++){
-         int curHt =heights[i];
-         start = i;
-        while(!stack.isEmpty() && stack.peek().getValue() > curHt){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (i - index));
-          start = index;
+       int n = heights.length;
+        int[] left = nsl(heights,n);
+        int[] right = nsr(heights,n);
+        int maxArea = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int area = (right[i]-left[i]-1)*heights[i];
+            maxArea = Math.max(maxArea,area);
         }
-        stack.push(new Pair(start,curHt));
-      }
-      
-       while(!stack.isEmpty()){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (n - index));
-       }
-        return area;
+        return maxArea; 
     }
+    static int[] nsl(int arr[], int n){
+        Stack<int[]> st = new Stack<>();
+        int idx=-1;
+        int[] left = new int[n];
+        for(int i=0;i<n;i++){
+            if(st.isEmpty()) left[i] = idx;
+            else if(!st.isEmpty() && st.peek()[0]<arr[i]) 
+                left[i] = st.peek()[1];
+            else if(!st.isEmpty() && st.peek()[0]>=arr[i]){
+                while(!st.isEmpty() && st.peek()[0]>=arr[i]) st.pop();
+                if(st.isEmpty()) left[i] = idx;
+                else left[i] = st.peek()[1];
+            }
+            st.push(new int[]{arr[i],i});
+        }
+        return left;
+    }
+    static int[] nsr(int arr[], int n){
+        Stack<int[]> st = new Stack<>();
+        int idx=n;
+        int[] right = new int[n];
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty()) right[i] = idx;
+            else if(!st.isEmpty() && st.peek()[0]<arr[i]) 
+                right[i] = st.peek()[1];
+            else if(!st.isEmpty() && st.peek()[0]>=arr[i]){
+                while(!st.isEmpty() && st.peek()[0]>=arr[i]) st.pop();
+                if(st.isEmpty()) right[i] = idx;
+                else right[i] = st.peek()[1];
+            }
+            st.push(new int []{arr[i],i});
+        }
+        return right;
+    } 
 }
